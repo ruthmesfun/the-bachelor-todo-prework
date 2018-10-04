16e8836608d0952b2541ee5a9f244307331c8b78
ruthmesfun <ruth.mesfun@gmail.com>
2018-10-03 22:24:56 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,48 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season][0]["name"].split(' ')[0]
 end
-
 def get_contestant_name(data, occupation)
-  # code here
+
+  contestant = data.values.each do |seasonal_data|
+    seasonal_data.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_contestant = []
+  contestant = data.values.each do |seasonal_data|
+    seasonal_data.each do |contestant|
+      if contestant["hometown"] == hometown
+         hometown_contestant << contestant
+      end
+    end
+  end
+
+  hometown_contestant.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant = data.values.each do |seasonal_data|
+    seasonal_data.each do |contestant|
+      if contestant["hometown"] == hometown
+         return contestant["occupation"]
+      end
+    end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0
+  ages_array = []
+  # data[season].each{|contestant| ages += contestant["age"].to_i}
+  data[season].each{|contestant| ages_array << contestant["age"].to_i}
+
+  average_age= ages_array.inject(:+)/ ages_array.length.to_f
+
+  average_age.round
 end
